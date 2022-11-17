d326a374250848674ffdc956032dfa8201bf8ba4
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-11-17 12:21:53 +0330

    handle adding ingredients

--- src/components/ingeredients/IngredientsForm.js
+++ src/components/ingeredients/IngredientsForm.js
@@ -1,18 +1,32 @@
 import Card from "../UI/Card";
 import classes from './IngeredientsForm.module.css';
+import {useState} from "react";
 
-const IngredientsForm = () => {
+const IngredientsForm = ({onAddIngredients}) => {
+    const [enteredName,setEnteredName] = useState('');
+    const [enteredAmount,setEnteredAmount]=useState('');
+
+    const nameChangeHandler = (event) => {
+        setEnteredName(event.target.value);
+    };
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+    const onAddIngredientsHandler = (event) => {
+        event.preventDefault();
+        onAddIngredients({id:enteredName,name:enteredName,amount:enteredAmount});
+    }
     return(
         <section className={classes['ingredient-form']}>
             <Card>
-                    <form>
+                    <form onSubmit={onAddIngredientsHandler}>
                         <div className={classes['form-control']}>
                             <label htmlFor='name'> Name </label>
-                            <input id='name' type='text'/>
+                            <input id='name' type='text' value={enteredName} onChange={nameChangeHandler}/>
                         </div>
                         <div className={classes['form-control']}>
                             <label htmlFor='amount'> Amount </label>
-                            <input id='amount' type='number'/>
+                            <input id='amount' type='number' value={enteredAmount} onChange={amountChangeHandler}/>
                         </div>
                         <div className={classes['ingredient-form__actions']}>
                             <button> Add Ingredient</button>
